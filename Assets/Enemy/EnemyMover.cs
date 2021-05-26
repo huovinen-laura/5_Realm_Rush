c00e45b176599a1b00cb38bdde07aab5ac5b56e5
Laura Huovinen <laura.huovinen@outlook.com>
2021-05-26 22:09:17 +0300

    Assets and prefab variants
    - Imported assets
    - Created prefab variants

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] private List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] private float waitTime = 1f;
 
     void Start()
     {
-        PrintWaypointName();
+        StartCoroutine(FollowPath());
     }
 
-    void PrintWaypointName()
+    IEnumerator FollowPath()
     {
         foreach (Waypoint waypoint in path)
         {
-            Debug.Log(waypoint.name);
+            transform.position = waypoint.transform.position;
+
+            // Ends stuff here, goes back to start to finish it and
+            // returns after 1 second to continue here.
+            // Loops until every waypoint has been visited.
+            yield return new WaitForSeconds(waitTime);
         }
     }
 }
